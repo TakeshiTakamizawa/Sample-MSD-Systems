fb2edc845857c51478fdc5f78dcd30311ef781e2
TakeshiTakamizawa <t.takeshi0624@outlook.com>
2025-10-23 14:00:55 +0900

    スタート

--- MSD_dyn.m
+++
@@ -1,6 +0,0 @@
-function Z_dot = MSD_dyn(~, Z, m, c, k)
-    x = ; 
-    x_dot = ;
-    x_2dot = ;
-    Z_dot = ;
-end
---
+++ sample_MSD_RK4/MSD_dyn.m
@@ -0,0 +1,6 @@
+function Z_dot = MSD_dyn(~, Z, m, c, k)
+    x = ; 
+    x_dot = ;
+    x_2dot = ;
+    Z_dot = ;
+end
